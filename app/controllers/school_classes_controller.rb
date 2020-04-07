5db96cb5d2983ff4e3dc156b21d95392422031cb
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-04-07 22:33:28 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -21,7 +21,7 @@ class SchoolClassesController < ApplicationController
 	def update
 	  @class = SchoolClass.find(params[:id])
     @class.update(school_class_params(:title, :room_number))
-    redirect_to school_class_path(@school_class)
+    redirect_to school_class_path(@class)
   end
   
   
